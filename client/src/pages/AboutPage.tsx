d323c006c5854df0d5290d0e9878b22cdebde7d1
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-13 18:27:05 +0900

    page

---
+++ client/src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+// src/pages/About.tsx
+import React from "react"
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the about page! Here we describe our mission and values.</p>
+    </div>
+  )
+}
+
+export default About
