6d8c457d1b9699a644473c2c132fb2444382e025
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-14 19:59:13 +0900

    example page

--- client/src/pages/ExamplePage.tsx
+++ client/src/pages/ExamplePage.tsx
@@ -1,36 +1,63 @@
-const people = [
-  {
-    name: "Calvin Hawkins",
-    email: "calvin.hawkins@example.com",
-    image:
-      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Kristen Ramos",
-    email: "kristen.ramos@example.com",
-    image:
-      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Ted Fox",
-    email: "ted.fox@example.com",
-    image:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-]
+import reactLogo from "../assets/react.svg"
+import viteLogo from "/vite.svg"
+import { KeyPair, SymbolFacade } from "symbol-sdk/symbol"
+import { Network } from "symbol-sdk/nem"
+import { PrivateKey } from "symbol-sdk"
 
-export default function Example() {
+function Index() {
+  const onClick = () => {
+    const privateKey = import.meta.env.VITE_PRIVATE_KEY
+    const account = new KeyPair(new PrivateKey(privateKey))
+    const message = new Uint8Array([
+      0x00,
+      ...new TextEncoder().encode("test transaction"),
+    ])
+
+    const facade = new SymbolFacade(Network.TESTNET.toString())
+
+    const tx = facade.transactionFactory.create({
+      type: "transfer_transaction_v1",
+      signerPublicKey: account.publicKey,
+      deadline: facade.now().addHours(2).timestamp,
+      recipientAddress: facade.network.publicKeyToAddress(account.publicKey),
+      mosaics: [],
+      message,
+      fee: 1000000n,
+    })
+    const signature = facade.signTransaction(account, tx)
+    const jsonPayload = facade.transactionFactory.static.attachSignature(
+        tx,
+        signature,
+    )
+
+    console.log({ jsonPayload })
+
+    fetch(
+        new URL("/transactions", "https://sym-test-03.opening-line.jp:3001"),
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: jsonPayload,
+        },
+    ).then((res) => res.json())
+  }
+
   return (
-    <ul className='divide-y divide-gray-200'>
-      {people.map((person) => (
-        <li key={person.email} className='py-4 flex'>
-          <img className='size-10 rounded-full' src={person.image} alt='' />
-          <div className='ml-3'>
-            <p className='text-sm font-medium text-gray-900'>{person.name}</p>
-            <p className='text-sm text-gray-500'>{person.email}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+      <>
+        <div>
+          <a href='https://vitejs.dev' target='_blank'>
+            <img src={viteLogo} className='logo' alt='Vite logo' />
+          </a>
+          <a href='https://react.dev' target='_blank'>
+            <img src={reactLogo} className='logo react' alt='React logo' />
+          </a>
+        </div>
+        <h1>DEMO APP</h1>
+        <div className='card'>
+          <button onClick={onClick}>click</button>
+        </div>
+      </>
   )
 }
+
+export default Index
