9e3578299a6664014fa5c987f355c5684a0854f1
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-14 20:27:04 +0900

    navbar

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,19 +1,42 @@
 import React from "react"
 import { Outlet, Link } from "react-router-dom"
+import { Menu } from "@headlessui/react"
 
 const Layout: React.FC = () => {
   return (
     <div>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/list'>List</Link>
-            </li>
-            <li>
-              <Link to='/new'>New</Link>
-            </li>
-          </ul>
+        <nav className='bg-gray-800'>
+          <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+            <div className='flex justify-between items-center h-16'>
+              <div className='flex space-x-4'>
+                <Menu>
+                  <div className='flex space-x-4'>
+                    <Menu.Item>
+                      {({ focus }) => (
+                        <Link
+                          to='/list'
+                          className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${focus ? "bg-gray-700 text-white" : ""}`}
+                        >
+                          List
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ focus }) => (
+                        <Link
+                          to='/new'
+                          className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${focus ? "bg-gray-700 text-white" : ""}`}
+                        >
+                          New
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu>
+              </div>
+            </div>
+          </div>
         </nav>
       </header>
       <main>
