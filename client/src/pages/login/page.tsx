722347c476e6182337a08ff41fc10160a11b1738
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-15 16:20:01 +0900

    秘密鍵見えるボタン

--- client/src/pages/login/page.tsx
+++ client/src/pages/login/page.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid"
 
 function LoginPage() {
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
   const navigate = useNavigate()
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -20,9 +22,11 @@ function LoginPage() {
     if (value.length <= 64 && /^[0-9a-fA-F]*$/.test(value)) {
       setPassword(value)
       setError("")
-    } else {
-      setError("秘密鍵は64文字の16進数である必要があります。")
     }
+  }
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword)
   }
 
   const isValidHex = (str: string) => /^[0-9a-fA-F]{64}$/.test(str)
@@ -33,13 +37,25 @@ function LoginPage() {
         <h1 className='text-2xl font-bold text-center'>ログイン</h1>
         <form onSubmit={handleSubmit}>
           <div className='mb-4'>
-            <label className='block text-gray-700'>秘密鍵</label>
-            <input
-              type='password'
-              value={password}
-              onChange={handleChange}
-              className='w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
-            />
+            <label className='block text-gray-700 mb-2'>秘密鍵（16進数64文字）</label>
+            <div className='relative'>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={handleChange}
+                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+              />
+              <div
+                className='absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer'
+                onClick={toggleShowPassword}
+              >
+                {showPassword ? (
+                  <EyeIcon className='h-5 w-5 text-gray-500' />
+                ) : (
+                  <EyeSlashIcon className='h-5 w-5 text-gray-500' />
+                )}
+              </div>
+            </div>
             {error && <p className='text-red-500 text-sm mt-2'>{error}</p>}
           </div>
           <button
