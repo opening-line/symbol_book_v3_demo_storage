1ac87646598fae816fce964820806aa667a86c2d
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-15 17:44:11 +0900

    lint

--- client/src/pages/register/page.tsx
+++ client/src/pages/register/page.tsx
@@ -41,9 +41,7 @@ function GenerateKeyPage() {
           </div>
         )}
         <div className='mb-4'>
-          <label className='block text-gray-700 mb-2'>
-            生成されたアドレス
-          </label>
+          <label className='block text-gray-700 mb-2'>生成されたアドレス</label>
           <div className='relative'>
             <input
               type='text'
@@ -65,7 +63,7 @@ function GenerateKeyPage() {
         >
           違う鍵を生成する
         </button>
-        <hr className='my-4 border-t-2 border-gray-300'/>
+        <hr className='my-4 border-t-2 border-gray-300' />
         <button
           onClick={() => navigate("/")}
           className='w-full px-4 py-2 mt-4 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400'
