fff8b4b43d5cd084b3939dcf27fafa2357bae0bc
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-21 17:24:52 +0900

    detail image show no_image.svg when unknown mime

--- client/src/pages/detail/page.tsx
+++ client/src/pages/detail/page.tsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom"
 import useGetImageFromBlockchain from "../../hooks/useGetImageFromBlockchain.ts"
+import { useMemo } from "react"
+import mime from "mime"
 
 const Detail = () => {
   const { id } = useParams<{ id: string }>()
 
   const { data, loading, error } = useGetImageFromBlockchain(id)
+
+  const mimeType = useMemo(() => {
+    if (data && data.meta && data.meta.fileName) {
+      return mime.getType(data.meta.fileName)
+    }
+
+    return null
+  }, [data])
 
   return (
     <div className='container mx-auto'>
@@ -17,8 +27,13 @@ const Detail = () => {
         <p>Error loading image</p>
       ) : (
         <>
-          {data?.payload && (
-            <img src={`data:image/png;base64,${data?.payload}`} alt='Preview' />
+          {data && data.payload && mimeType ? (
+            <img
+              src={`data:${mimeType};base64,${data.payload}`}
+              alt='Preview'
+            />
+          ) : (
+            <img src='/no_image.svg' alt='Preview' />
           )}
         </>
       )}
