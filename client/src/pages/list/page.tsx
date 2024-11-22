48c0a76a73fe3c02e0a46f3a8cc89b6a4fe2a1ca
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-22 18:30:11 +0900

    lint

--- client/src/pages/list/page.tsx
+++ client/src/pages/list/page.tsx
@@ -71,7 +71,7 @@ const ImageGallery: React.FC = () => {
       </div>
       <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-4'>
         {idList.map((id) => (
-          <ImageListItem key={id} fileId={id} onClick={handleImageClick}/>
+          <ImageListItem key={id} fileId={id} onClick={handleImageClick} />
         ))}
       </div>
       <div>
