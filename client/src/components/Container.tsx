0b6a2c36b210e8f4289d257631e3dce1db8ac435
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-26 14:19:59 +0900

    text-centerやめる

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className='container mx-auto text-center pt-4 pb-10'>{children}</div>
+    <div className='container mx-auto pt-4 pb-10'>{children}</div>
   )
 }
 
