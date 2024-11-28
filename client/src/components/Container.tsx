d11ea63328f8e649f560220abe0ad3072fce6571
planethouki <26595148+planethouki@users.noreply.github.com>
2024-11-28 13:45:22 +0900

    sp画面、px-1

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className='container mx-auto pt-4 pb-10'>{children}</div>
+  return <div className='container mx-auto pt-4 pb-10 px-1 sm:px-0'>{children}</div>
 }
 
 export default Container
