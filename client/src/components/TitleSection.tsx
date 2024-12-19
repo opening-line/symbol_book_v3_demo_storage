71c35c761ce2799e27ba1cceaeb040d5fc1226b3
planethouki <26595148+planethouki@users.noreply.github.com>
2024-12-19 11:16:49 +0900

    TitleSection

--- client/src/components/TitleSection.tsx
+++ client/src/components/TitleSection.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import styles from "./TitleSection.module.css"
 
 interface Props {
   children: React.ReactNode
 }
 
 const TitleSection: React.FC<Props> = ({ children }) => {
-  return <h1 className='text-2xl font-bold mb-8'>{children}</h1>
+  return <h1 className={styles.title}>{children}</h1>
 }
 
 export default TitleSection
