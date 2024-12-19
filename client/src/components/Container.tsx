f748c977439375dabf2c61734c99edbeab0cc01b
planethouki <26595148+planethouki@users.noreply.github.com>
2024-12-19 11:15:40 +0900

    Container

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,15 +1,12 @@
 import React from "react"
+import styles from "./Container.module.css"
 
 interface ContainerProps {
   children: React.ReactNode
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return (
-    <div className='container mx-auto pt-4 pb-10 px-1 sm:px-0'>
-      {children}
-    </div>
-  )
+  return <div className={styles.container}>{children}</div>
 }
 
 export default Container
