81ab55b6549eaa8146dc14b849a5d80261c9de18
planethouki <26595148+planethouki@users.noreply.github.com>
2024-12-19 11:38:17 +0900

    Layout

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -12,6 +12,7 @@ import usePrivateKeyStorage from "../hooks/usePrivateKeyStorage.ts"
 import { KeyPair, SymbolFacade } from "symbol-sdk/symbol"
 import { PrivateKey } from "symbol-sdk"
 import { Config } from "../utils/config.ts"
+import styles from "./Layout.module.css"
 
 const Layout: React.FC = () => {
   const navigate = useNavigate()
@@ -52,45 +53,39 @@ type NavbarProps = {
 
 function Navbar({ handleLogout, address }: NavbarProps) {
   return (
-    <nav className='bg-gray-800 text-white px-4 py-2 flex items-center justify-between'>
-      <div className='flex items-center space-x-6'>
-        <div className='text-lg font-bold flex-shrink-0'>
-          <span className='block sm:inline'>ブロックチェーン</span>
-          <span className='block sm:inline'>ストレージ</span>
+    <nav className={styles.navbar}>
+      <div className={styles.navLeft}>
+        <div className={styles.logo}>
+          <span>ブロックチェーン</span>
+          <span>ストレージ</span>
         </div>
 
-        <div className='flex space-x-4'>
-          <Link
-            to='/list'
-            className='text-sm font-medium hover:text-gray-300'
-          >
+        <div className={styles.links}>
+          <Link className={styles.link} to='/list'>
             一覧
           </Link>
-          <Link
-            to='/new'
-            className='text-sm font-medium hover:text-gray-300'
-          >
+          <Link className={styles.link} to='/new'>
             新規
           </Link>
         </div>
       </div>
 
-      <Menu as='div' className='relative inline-block text-left'>
-        <MenuButton className='flex flex-col items-center justify-center w-10 h-10 bg-gray-700 text-white rounded-full text-xs font-bold'>
+      <Menu as='div' className={styles.menuContainer}>
+        <MenuButton className={styles.menuButton}>
           <span>{address.slice(0, 4)}</span>
           <span>{address.slice(-4)}</span>
         </MenuButton>
 
-        <MenuItems className='absolute right-0 mt-2 w-40 bg-white divide-y divide-gray-200 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-          <div className='py-1'>
+        <MenuItems className={styles.menuItems}>
+          <div>
             <MenuItem>
               {({ focus }) => (
                 <Button
                   as={Link}
                   to='/mypage'
-                  className={`${
-                    focus ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                  } block px-4 py-2 text-sm w-full text-left`}
+                  className={`${styles.menuItem} ${
+                    focus ? styles.menuItemFocus : ""
+                  }`}
                 >
                   マイページ
                 </Button>
@@ -100,9 +95,9 @@ function Navbar({ handleLogout, address }: NavbarProps) {
               {({ focus }) => (
                 <Button
                   onClick={handleLogout}
-                  className={`${
-                    focus ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                  } block px-4 py-2 text-sm w-full text-left`}
+                  className={`${styles.menuItem} ${
+                    focus ? styles.menuItemFocus : ""
+                  }`}
                 >
                   ログアウト
                 </Button>
